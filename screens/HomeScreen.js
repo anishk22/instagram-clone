69f73b4f0eacdcd17777324c93ba43f32244e0d4
anishvk <anishvk@umich.edu>
2022-06-04 15:52:45 -0400

    basic header file

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,20 @@
-import * as React from 'react';
-import { View, Text } from 'react-native';
+import React from 'react';
+import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
+import Header from '../components/home/Header';
 
 const HomeScreen = () => {
     return (
-        <View>
-         <Text>HOMESCREEN</Text>
-        </View>
+        <SafeAreaView style ={styles.container}>
+            <Header />
+        </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    container: { 
+        backgroundColor: 'black',
+        flex: 1,
+    },
+})
 
 export default HomeScreen
