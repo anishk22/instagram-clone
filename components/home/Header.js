a42c634723771c243c836e38f59f503341040cac
anishvk <anishvk@umich.edu>
2022-06-04 16:02:33 -0400

    finished header component

--- components/home/Header.js
+++ components/home/Header.js
@@ -31,6 +31,10 @@ const Header = () => {
                 </TouchableOpacity>
 
                 <TouchableOpacity>
+                    <View style={styles.unreadBadge}>
+                        <Text style={styles.unreadBadgeText}>11</Text>
+                    </View>
+
                     <Image
                         source={{
                             uri: 'https://img.icons8.com/fluency-systems-regular/60/ffffff/facebook-messenger.png'
@@ -66,6 +70,24 @@ const styles = StyleSheet.create({
         height: 30,
         marginLeft: 10,
         resizeMode: 'contain',
+    },
+
+    unreadBadge: {
+        backgroundColor: '#FF3250',
+        position: 'absolute',
+        left: 20,
+        bottom: 18,
+        width: 25,
+        height: 18,
+        borderRadius: 25,
+        alignItems: 'center',
+        justifyContent: 'center',
+        zIndex: 1,
+    },
+
+    unreadBadgeText: {
+        color: 'white',
+        fontWeight: '600',
     }
 })
 
