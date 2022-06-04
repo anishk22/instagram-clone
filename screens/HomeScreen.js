2fc0689071622303b1bbc9060ccb00008b525ea6
anishvk <anishvk@umich.edu>
2022-06-04 10:55:55 -0400

    homescreen basic thing compiles now

---
+++ screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+
+const HomeScreen = () => {
+    return (
+        <View>
+         <Text>HOMESCREEN</Text>
+        </View>
+    )
+}
+
+export default HomeScreen
