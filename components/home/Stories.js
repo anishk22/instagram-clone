2f7506e15e881988b60cbed8b553c3f8acdbc844
anishvk <anishvk@umich.edu>
2022-06-04 17:15:53 -0400

    finished stories component

---
+++ components/home/Stories.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { View, Text, ScrollView, Image, StyleSheet } from 'react-native'
+import { Users } from '../../data/users' 
+
+const Stories = () => {
+  return (
+    <View style={{marginBottom: 13}}>  
+        <ScrollView 
+            horizontal
+            showsHorizontalScrollIndicator={false}
+        > 
+            {Users.map((story, index) => (
+                <View key={index} style={{ alignItems: 'center' }}>
+                    <Image 
+                        source={{uri: story.image}}
+                        style={styles.story}
+                    /> 
+                    <Text style={{color: 'white'}}>
+                        {story.user.length > 11 
+                        ? story.user.slice(0, 10).toLowerCase() + '...' 
+                        : story.user.toLowerCase()}
+                    </Text>
+                </View>
+            ))}
+
+        </ScrollView>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    story: {
+        width: 70,
+        height: 70,
+        borderRadius: 50,
+        marginLeft: 6,
+        borderWidth: 3,
+        borderColor: '#d62996'
+    },
+}) 
+
+export default Stories
---
+++ data/users.js
@@ -0,0 +1,27 @@
+export const Users = [
+    {
+        user: 'pokemon',
+        image: 
+            'https://cdn1.vectorstock.com/i/1000x1000/74/05/pokemon-go-logo-icon-vector-23237405.jpg',
+    },
+    {
+        user: 'postmalone',
+        image: 
+            'https://www.nme.com/wp-content/uploads/2019/09/Post-Malone.jpg',
+    },
+    {
+        user: 'nfl',
+        image: 
+            'https://i0.wp.com/sportytell.com/wp-content/uploads/2020/11/NFL-Team-Logos.jpg?fit=1300%2C675&ssl=1',
+    },
+    {
+        user: 'apple',
+        image: 
+            'https://rctechworld.com/wp-content/uploads/2022/02/apple.jpg',
+    },
+    {
+        user: 'uofmichigan',
+        image: 
+            'https://www.med.umich.edu/ott/mm/images/block-m-feed.jpg',
+    },
+]
