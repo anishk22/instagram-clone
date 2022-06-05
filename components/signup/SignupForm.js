2d208d29ff65ec8b8f5e1e12c911811304c52ea5
anishvk <anishvk@umich.edu>
2022-06-05 16:04:06 -0400

    signup sent to firebase

--- components/signup/SignupForm.js
+++ components/signup/SignupForm.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react'
-import { View, Text, TextInput, StyleSheet, Pressable, TouchableOpacity } from 'react-native'
+import { View, Text, TextInput, StyleSheet, Pressable, TouchableOpacity, Alert } from 'react-native'
 import { Formik } from 'formik'
 import * as Yup from 'yup'
 import Validator from 'email-validator'
+import firebase from '../../firebase'
 
 const SignupForm = ({ navigation }) => {
     const SignupFormSchema = Yup.object().shape({
@@ -11,11 +12,22 @@ const SignupForm = ({ navigation }) => {
         password: Yup.string().required().min(8, 'Your password has to have at least 8 characters'),
     })
 
+    const onSignup = async (email, password) => {
+        try {
+            await firebase.auth().createUserWithEmailAndPassword(email, password)
+            console.log('User created successfully!', email, password)
+        } 
+        catch (error) {
+            Alert.alert(error.message)
+        }
+    }
+
     return (
         <View style={styles.wrapper}>
             <Formik
                 initialValues={{email: '', username: '', password: ''}}
                 onSubmit={values => {
+                    onSignup(values.email, values.password)
                     console.log(values)
                 }}
                 validationSchema={SignupFormSchema}
